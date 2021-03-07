5b98c3efb5bbb5792f3bff35f1997c4fa9178c5a
Neo238 <neo2380@protonmail.com>
2021-03-07 14:01:06 +0000

    Pre-Alpha 0.0.4 - Teleporters ready and some other tweaks.

--- Project/scripts/bobbing.gd
+++ Project/scripts/bobbing.gd
@@ -1,7 +1,5 @@
 extends StaticBody2D
-
 # Brings title up slowly
-# 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Project/scripts/teleporters/tele1.gd
+++ Project/scripts/teleporters/tele1.gd
@@ -3,5 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	 pass
 
+
+func _on_Area2D2_body_entered(_body):
+	print("Area entered")
+	var _cs = get_tree().change_scene("res://scenes/levels/Lvl2.tscn")
